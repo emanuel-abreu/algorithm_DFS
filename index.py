28a13b2a54626ef46a3e18172f925676b319cb65
Emanuel Abreu <emanuel_abreu.2@outlook.com>
2023-11-19 17:40:15 -0300

    correção na lógica de criação da lista de adjcênci

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-entrada = open("G1.txt", "r")
+entrada = open("G2.txt", "r")
 
 linha = entrada.readlines()
 grafoDeEntrada = linha[0].split()
@@ -15,11 +15,15 @@ nomeDoGrafo = grafoDeEntrada[2]
 for i in range(1, len(linha)):
     vetorDaLinha = linha[i].split()
 
-    if (linha[i][0] not in listaDeVertices):
+    if linha[i][0] not in listaDeVertices:
         relacionamentoDosVertices[linha[i][0]] = []
         listaDeVertices.append(linha[i][0])
+    elif linha[i][1] not in listaDeVertices:
+        relacionamentoDosVertices[linha[i][1]] = []
+        listaDeVertices.append(linha[i][1])
 
-    relacionamentoDosVertices[linha[i][0]].append(linha[i][1])
+    if linha[i][1] not in relacionamentoDosVertices[linha[i][0]]:
+        relacionamentoDosVertices[linha[i][0]].append(linha[i][1])
 
 listaDeVertices.sort(key=lambda v: len(relacionamentoDosVertices[v]), reverse=True)
 relacionamentoDosVerticesOrdenado = dict(sorted(relacionamentoDosVertices.items()))
@@ -36,6 +40,7 @@ def DFS(G):
 
     for u in G:
         if cor[u] == "BRANCO":
+            print("vertice u: ", u)
             DFS_VISIT(u)
     
     
@@ -43,15 +48,21 @@ def DFS_VISIT(u):
     global mark
     cor[u] = "CINZA"
     mark = mark + 1
+    print("mark cinza: ",mark)
+    print(cor)
     vetorD.append(mark)
 
 
     for v in relacionamentoDosVerticesOrdenado[u]:
         if cor[v] == "BRANCO":
+            print("vertice v: ", v)
             DFS_VISIT(v)
     
     cor[u] = "PRETO"
+    print(u)
+    print(cor)
     mark = mark + 1
+    print("mark preto: ",mark,"\n\n")
     vetorF.append(mark)
 
 DFS(listaDeVertices)
