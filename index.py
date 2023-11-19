798c24ca25eacd770667494a70535bde7a56ada9
Emanuel Abreu <emanuel_abreu.2@outlook.com>
2023-11-19 15:57:33 -0300

    implementando o algotirmo DFS

--- index.py
+++ index.py
@@ -12,10 +12,6 @@ numeroVertices = int(grafoDeEntrada[0])
 numeroArestas = int(grafoDeEntrada[1])
 nomeDoGrafo = grafoDeEntrada[2]
 
-print(numeroVertices)
-print(numeroArestas)
-print(nomeDoGrafo+"\n")
-
 for i in range(1, len(linha)):
     vetorDaLinha = linha[i].split()
 
@@ -25,7 +21,43 @@ for i in range(1, len(linha)):
 
     relacionamentoDosVertices[linha[i][0]].append(linha[i][1])
 
+listaDeVertices.sort(key=lambda v: len(relacionamentoDosVertices[v]), reverse=True)
 relacionamentoDosVerticesOrdenado = dict(sorted(relacionamentoDosVertices.items()))
+
 print(relacionamentoDosVerticesOrdenado)
+print(listaDeVertices)
+
+cor = {}
+def DFS(G):
+    global mark
+    for u in G:
+        cor[u] = "BRANCO"
+    mark = 0
+
+    for u in G:
+        if cor[u] == "BRANCO":
+            DFS_VISIT(u)
+    
+    
+def DFS_VISIT(u):
+    global mark
+    cor[u] = "CINZA"
+    mark = mark + 1
+    vetorD.append(mark)
+
+
+    for v in relacionamentoDosVerticesOrdenado[u]:
+        if cor[v] == "BRANCO":
+            DFS_VISIT(v)
+    
+    cor[u] = "PRETO"
+    mark = mark + 1
+    vetorF.append(mark)
+
+DFS(listaDeVertices)
+
+print("vetor d:",  vetorD)
+print("vetor f:" , vetorF)
+
 
 entrada.close()
